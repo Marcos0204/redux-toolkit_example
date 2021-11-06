f304b13e89921d61b71422336e4cf2f4cea89ffc
Marcos Bastidas <marcosbastidas301@gmail.com>
2021-11-06 16:37:43 -0400

    update-components

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+// bootstrap
+import "bootstrap/dist/css/bootstrap.css";
+
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-dark bg-dark">
+            <div className="container">
+                <a className="navbar-brand" href="/">React Redux App</a>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
